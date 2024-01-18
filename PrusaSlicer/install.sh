087f3e67d1ea6601df098ac8a37b90051e02b3e2
Michael Telatynski <7t3chguy@gmail.com>
2024-01-18 01:43:45 +0000

    config

--- PrusaSlicer/install.sh
+++ PrusaSlicer/install.sh
@@ -25,7 +25,7 @@ StartupWMClass=PrusaSlicerDesktop
 Type=Application
 EOL
 chmod +x /usr/share/applications/prusaslicer.desktop
-mkdir /home/kasm-user/Desktop
-cp /usr/share/applications/prusaslicer.desktop /home/kasm-user/Desktop/prusaslicer.desktop
+mkdir /config/Desktop
+cp /usr/share/applications/prusaslicer.desktop /config/Desktop/prusaslicer.desktop
 
-chown 1000:0 /home/kasm-user/Desktop/prusaslicer.desktop
+chown 1000:0 /config/Desktop/prusaslicer.desktop
